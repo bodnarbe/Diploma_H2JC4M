bd22d5afe6417a403fd6eb1def5459d61a79f9f7
Bodnár Bence Tibor <bodnarbe94@gmail.com>
2023-11-30 15:21:25 +0100

    STM firmware added

---
+++ Telemanipulator_2023/v1/Core/Inc/main.h
@@ -0,0 +1,89 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define B1_Pin GPIO_PIN_12
+#define B1_GPIO_Port GPIOB
+#define B2_Pin GPIO_PIN_13
+#define B2_GPIO_Port GPIOB
+#define CS1_Pin GPIO_PIN_3
+#define CS1_GPIO_Port GPIOB
+#define CS2_Pin GPIO_PIN_4
+#define CS2_GPIO_Port GPIOB
+#define CS3_Pin GPIO_PIN_5
+#define CS3_GPIO_Port GPIOB
+#define CS4_Pin GPIO_PIN_6
+#define CS4_GPIO_Port GPIOB
+#define CS5_Pin GPIO_PIN_7
+#define CS5_GPIO_Port GPIOB
+#define CS6_Pin GPIO_PIN_8
+#define CS6_GPIO_Port GPIOB
+#define CS7_Pin GPIO_PIN_9
+#define CS7_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
